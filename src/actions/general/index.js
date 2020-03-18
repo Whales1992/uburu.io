595f4dfbda26f88a3ded965e8babf159c2ba3270
Daniel Ngozika <danielngozikac@gmail.com>
2020-03-18 08:47:13 +0100

    create initial actions.

---
+++ src/actions/general/index.js
@@ -0,0 +1,22 @@
+import { HIDE_ERROR_MODAL, SHOW_ERROR_MODAL } from "./constants";
+
+export const showErrorModal = message => ({
+	type: SHOW_ERROR_MODAL,
+	message
+});
+
+export const hideErrorModal = () => ({
+	type: HIDE_ERROR_MODAL
+});
+
+export const errorHandler = async (err, dispatch) => {
+	if (err.message === "Failed to fetch") {
+		dispatch(
+			showErrorModal("Connection lost. Check your network and retry.")
+		);
+		await setTimeout(() => dispatch(hideErrorModal()), 3000);
+	} else {
+		dispatch(showErrorModal(`${err.message}`));
+		await setTimeout(() => dispatch(hideErrorModal()), 3000);
+	}
+};
