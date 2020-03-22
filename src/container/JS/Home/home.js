e99669adf6667a945588f3edf5fd3188fdb73b23
Daniel Ngozika <danielngozikac@gmail.com>
2020-03-22 12:20:00 +0100

    Create account Home page.

---
+++ src/container/JS/Home/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Layout from "../../../components/UI/JS/layout";
+import BlueBackdrop from "../../../components/Home/JS/blue_backdrop";
+
+class Home extends Component {
+	render() {
+		return (
+			<Layout>
+				<BlueBackdrop />
+			</Layout>
+		);
+	}
+}
+
+export default Home;
