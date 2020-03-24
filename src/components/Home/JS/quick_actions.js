6a134b39d7855ca2b73fdecf8a6ea1077030e11f
Daniel Ngozika <danielngozikac@gmail.com>
2020-03-24 16:16:25 +0100

    Create quick action component.

---
+++ src/components/Home/JS/quick_actions.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../CSS/quick_actions.module.css";
+import AddPatientDataIcon from "../../../images/add_patient_data.svg";
+import SyncDataIcon from "../../../images/sync_data.svg";
+import CreateAppointmentIcon from "../../../images/create_appointments.svg";
+import UpdatePatientDataIcon from "../../../images/update_patient_data.svg";
+
+const quickActions = () => (
+	<div className={styles.div}>
+		<div className={styles.top}>Quick Actions</div>
+		<div className={styles.actions}>
+			<div>
+				<Link className={styles.link}>
+					<img src={AddPatientDataIcon} alt="add patient icon" />
+					Add <br /> Patient Data
+				</Link>
+			</div>
+			<div>
+				<Link className={styles.link}>
+					<img
+						src={CreateAppointmentIcon}
+						alt="create appointment icon"
+					/>
+					Create <br /> Appointment
+				</Link>
+			</div>
+			<div>
+				<img src={UpdatePatientDataIcon} alt="update data icon" />
+				Update <br /> Patient Data
+			</div>
+			<div>
+				<img src={SyncDataIcon} alt="sync data icon" />
+				Sync <br /> Data Online
+			</div>
+		</div>
+	</div>
+);
+
+export default quickActions;
