9957fe42369647027b36b4bafa151ef0c23ac614
Daniel Ngozika <danielngozikac@gmail.com>
2020-03-24 16:16:58 +0100

    Add quick action component.

--- src/container/JS/Home/home.js
+++ src/container/JS/Home/home.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Layout from "../../../components/UI/JS/layout";
 import BlueBackdrop from "../../../components/Home/JS/blue_backdrop";
+import QuickActions from "../../../components/Home/JS/quick_actions";
 
 class Home extends Component {
 	render() {
 		return (
 			<Layout>
 				<BlueBackdrop />
+				<QuickActions />
 			</Layout>
 		);
 	}
