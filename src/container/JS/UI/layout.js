bd462f790e32c1757de0152faa615afd6def81f2
Daniel Ngozika <danielngozikac@gmail.com>
2020-04-02 13:54:39 +0100

    Move Layout to containers.

---
+++ src/container/JS/UI/layout.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import Topbar from "../../../components/UI/JS/topbar";
+import BottomBar from "../../../components/UI/JS/bottom_toolbar";
+import "../../CSS/UI/layout.module.css";
+import LeftDrawer from "../../../components/UI/JS/left_drawer";
+import Backdrop from "../../../components/UI/JS/backdrop";
+
+class layout extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			leftDrawerOpen: false
+		};
+
+		this.onBackdropClick = this.onBackdropClick.bind(this);
+		this.openSideDrawer = this.openSideDrawer.bind(this);
+	}
+
+	onBackdropClick() {
+		this.setState({ leftDrawerOpen: false });
+	}
+
+	openSideDrawer() {
+		this.setState({ leftDrawerOpen: true });
+	}
+
+	render() {
+		return (
+			<>
+				<Topbar openDrawer={this.openSideDrawer} />
+				<LeftDrawer open={this.state.leftDrawerOpen} />
+				{this.state.leftDrawerOpen ? (
+					<Backdrop onClick={this.onBackdropClick} />
+				) : null}
+				{this.props.children}
+				<BottomBar />
+			</>
+		);
+	}
+}
+
+export default layout;
