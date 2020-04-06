8e68c90c68e440fff568fa4f56a119f41ea395ac
Daniel Ngozika <danielngozikac@gmail.com>
2020-04-06 15:00:17 +0100

    Add missing pieces.

--- src/components/Appointments/JS/appointment_detail.js
+++ src/components/Appointments/JS/appointment_detail.js
@@ -1,29 +1,47 @@
 import React from "react";
-import styles from "../CSS/appointment_detail.module.css";
+import { connect } from "react-redux";
 
-const appointmentDetail = ({ detail }) => (
-	<div className={styles.detail}>
-		<div>
-			<span className={styles.field_name}>Patient's name</span>
-			<span className={styles.value}>{detail.name}</span>
+//styles
+import styles from "../CSS/appointment_detail.module.css";
+import detailStyles from "../../../container/CSS/Profile/profile.module.css";
+
+//components
+import SecondaryBar from "../../UI/JS/secondary_navbar";
+
+const appointmentDetail = ({ appointment }) => (
+	<>
+		<SecondaryBar page_title={appointment.name} />
+		<div className={styles.detail}>
+			<div className={detailStyles.indiv_info}>
+				<span className={detailStyles.field_name}>Patient's name</span>
+				<span className={detailStyles.value}>{appointment.name}</span>
+			</div>
+			<div className={detailStyles.indiv_info}>
+				<span className={detailStyles.field_name}>Serial Number</span>
+				<span className={detailStyles.value}>
+					{appointment.serial_number}
+				</span>
+			</div>
+			<div className={detailStyles.indiv_info}>
+				<span className={detailStyles.field_name}>Age</span>
+				<span className={detailStyles.value}>{appointment.age}</span>
+			</div>
+			<div className={detailStyles.indiv_info}>
+				<span className={detailStyles.field_name}>Severity</span>
+				<span className={detailStyles.value}>
+					{appointment.severity}
+				</span>
+			</div>
+			<div className={detailStyles.indiv_info}>
+				<span className={detailStyles.field_name}>Date/Time</span>
+				<span className={detailStyles.value}>{appointment.date}</span>
+			</div>
 		</div>
-		<div>
-			<span className={styles.field_name}>Serial Number</span>
-			<span className={styles.value}>{detail.serial_number}</span>
-		</div>
-		<div>
-			<span className={styles.field_name}>Age</span>
-			<span className={styles.value}>{detail.age}</span>
-		</div>
-		<div>
-			<span className={styles.field_name}>Severity</span>
-			<span className={styles.value}>{detail.severity}</span>
-		</div>
-		<div>
-			<span className={styles.field_name}>Date/Time</span>
-			<span className={styles.value}>{detail.date}</span>
-		</div>
-	</div>
+	</>
 );
 
-export default appointmentDetail;
+const mapStateToProps = ({ general }) => ({
+	appointment: general.detailed
+});
+
+export default connect(mapStateToProps, null)(appointmentDetail);
