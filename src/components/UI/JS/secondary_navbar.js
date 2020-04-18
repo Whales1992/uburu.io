b8e6cd45f5b838fe017a17fdc2c17ff8c7c7c9b5
Daniel Ngozika <danielngozikac@gmail.com>
2020-04-18 07:17:35 +0100

    Add box shadow conditionally and remove bar on larger screens.

--- src/components/UI/JS/secondary_navbar.js
+++ src/components/UI/JS/secondary_navbar.js
@@ -3,8 +3,10 @@ import { withRouter } from "react-router-dom";
 import styles from "../CSS/secondary_navbar.module.css";
 import BackIcon from "../../../images/back_icon.svg";
 
-const secondaryNavbar = ({ page_title, history }) => (
-	<div className={styles.bar}>
+const secondaryNavbar = ({ page_title, history, shadow }) => (
+	<div
+		className={shadow ? [styles.bar, styles.shadow].join(" ") : styles.bar}
+	>
 		<img src={BackIcon} alt="back icon" onClick={() => history.goBack()} />
 		<div className={styles.title}>{page_title}</div>
 	</div>
