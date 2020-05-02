a84c502e1fcba15ec1cd1209069c0352062a477f
Daniel Ngozika <danielngozikac@gmail.com>
2020-05-02 07:16:03 +0100

    Create the more menu.

---
+++ src/components/UI/JS/more_menu.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "../CSS/more_menu.module.css";
+import MedAnalyticsIcon from "../../../images/MoreMenu/med_analytics.svg";
+import MessageCenterIcon from "../../../images/MoreMenu/message_center.svg";
+import PharmDBIcon from "../../../images/MoreMenu/pharm_DB.svg";
+import ContactSupportIcon from "../../../images/MoreMenu/contact_support.svg";
+import SettingsIcon from "../../../images/MoreMenu/settings.svg";
+import AboutIcon from "../../../images/MoreMenu/about.svg";
+
+const moreMenu = () => (
+	<div className={styles.container}>
+		<div className={styles.link_div}>
+			<NavLink
+				to="/medical_analytics"
+				className={styles.link}
+				activeClassName={styles.active}
+			>
+				<img src={MedAnalyticsIcon} alt="bar chart icon" />
+				Medical Analytics
+			</NavLink>
+		</div>
+		<div className={styles.link_div}>
+			<NavLink
+				to="/message_center"
+				className={styles.link}
+				activeClassName={styles.active}
+			>
+				<img src={MessageCenterIcon} alt="chat icon" />
+				Message Center
+			</NavLink>
+		</div>
+		<div className={styles.link_div}>
+			<NavLink
+				to="/pharmacy_db"
+				className={styles.link}
+				activeClassName={styles.active}
+			>
+				<img src={PharmDBIcon} alt="friends icon" />
+				Pharmacy DB
+			</NavLink>
+		</div>
+		<div className={styles.link_div}>
+			<NavLink
+				to="/support"
+				className={styles.link}
+				activeClassName={styles.active}
+			>
+				<img src={ContactSupportIcon} alt="person profile icon" />
+				Contact Support
+			</NavLink>
+		</div>
+		<div className={styles.link_div}>
+			<NavLink
+				to="/settings"
+				className={styles.link}
+				activeClassName={styles.active}
+			>
+				<img src={SettingsIcon} alt="settings icon" />
+				Settings
+			</NavLink>
+		</div>
+		<div className={styles.link_div}>
+			<NavLink
+				to="/about"
+				className={styles.link}
+				activeClassName={styles.active}
+			>
+				<img src={AboutIcon} alt="alert icon" />
+				About BlueCircle
+			</NavLink>
+		</div>
+	</div>
+);
+
+export default moreMenu;
--- src/components/UI/JS/topbar.js
+++ src/components/UI/JS/topbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "../CSS/topbar.module.css";
 import Hamburger from "../../../images/hamburger.svg";
 import NotificationIcon from "../../../images/notification_icon.svg";
@@ -11,92 +11,120 @@ import AppointmentsActive from "../../../images/appointments_active.svg";
 import AppointmentsInactive from "../../../images/topbar_appointments_inactive.svg";
 import MoreIcon from "../../../images/more_icon.svg";
 import TopBarAvatar from "../../../images/topbar_avatar.svg";
+import MoreMenu from "./more_menu";
 import { NavLink } from "react-router-dom";
 
-const topbar = ({ page_title, openDrawer, hide_on_small_screens }) => (
-	<div
-		className={
-			hide_on_small_screens
-				? [styles.topBar, styles.hide].join(" ")
-				: styles.topBar
-		}
-	>
-		<img
-			className={styles.mobile_menu}
-			src={Hamburger}
-			alt="hamburger icon"
-			onClick={() => openDrawer()}
-		/>
-		<div className={styles.mobile_menu}>{page_title}</div>
-		<img
-			className={styles.mobile_menu}
-			src={NotificationIcon}
-			alt="notification icon"
-		/>
-		<>
-			<div className={styles.logo_div}>
-				<img src={TopBarLogo} alt="logo" />
-			</div>
-			<div className={styles.links_div}>
-				<NavLink
-					to="/"
-					className={styles.nav_link}
-					activeClassName={styles.nav_link_active}
-				>
-					{window.location.pathname === "/" ? (
-						<img src={HomeIconActive} alt="active home icon" />
-					) : (
-						<img src={HomeIconInactive} alt="inactive home icon" />
-					)}
-					Home
-				</NavLink>
-				<NavLink
-					to="/patients"
-					className={styles.nav_link}
-					activeClassName={styles.nav_link_active}
-				>
-					{window.location.pathname === "/patients" ? (
-						<img
-							src={PatientsIconActive}
-							alt="active patients icon"
-						/>
-					) : (
-						<img
-							src={PatientsIconInactive}
-							alt="inactive patients icon"
-						/>
-					)}
-					Patients
-				</NavLink>
-				<NavLink
-					to="/appointments"
-					className={styles.nav_link}
-					activeClassName={styles.nav_link_active}
-				>
-					{window.location.pathname === "/appointments" ? (
-						<img
-							src={AppointmentsActive}
-							alt="active appointments icon"
-						/>
-					) : (
-						<img
-							src={AppointmentsInactive}
-							alt="inactive appointments icon"
-						/>
-					)}
-					Appointments
-				</NavLink>
-				<div className={styles.more_div}>
-					<img src={MoreIcon} alt="more icon" />
-					<span>More</span>
-				</div>
-			</div>
-			<div className={styles.avatar_div}>
-				<img src={TopBarAvatar} alt="avatar" />
-				<span>Dr. Nuel</span>
-			</div>
-		</>
-	</div>
-);
+class Topbar extends Component {
+	constructor(props) {
+		super(props);
 
-export default topbar;
+		this.state = {
+			moreMenuShowing: false,
+		};
+	}
+
+	toggleMoreMenu() {
+		this.setState((state) => ({ moreMenuShowing: !state.moreMenuShowing }));
+	}
+
+	render() {
+		const { hide_on_small_screens, openDrawer, page_title } = this.props;
+		return (
+			<div
+				className={
+					hide_on_small_screens
+						? [styles.topBar, styles.hide].join(" ")
+						: styles.topBar
+				}
+			>
+				<img
+					className={styles.mobile_menu}
+					src={Hamburger}
+					alt="hamburger icon"
+					onClick={() => openDrawer()}
+				/>
+				<div className={styles.mobile_menu}>{page_title}</div>
+				<img
+					className={styles.mobile_menu}
+					src={NotificationIcon}
+					alt="notification icon"
+				/>
+				<>
+					<div className={styles.logo_div}>
+						<img src={TopBarLogo} alt="logo" />
+					</div>
+					<div className={styles.links_div}>
+						<NavLink
+							to="/"
+							className={styles.nav_link}
+							activeClassName={styles.nav_link_active}
+						>
+							{window.location.pathname === "/" ? (
+								<img
+									src={HomeIconActive}
+									alt="active home icon"
+								/>
+							) : (
+								<img
+									src={HomeIconInactive}
+									alt="inactive home icon"
+								/>
+							)}
+							Home
+						</NavLink>
+						<NavLink
+							to="/patients"
+							className={styles.nav_link}
+							activeClassName={styles.nav_link_active}
+						>
+							{window.location.pathname === "/patients" ? (
+								<img
+									src={PatientsIconActive}
+									alt="active patients icon"
+								/>
+							) : (
+								<img
+									src={PatientsIconInactive}
+									alt="inactive patients icon"
+								/>
+							)}
+							Patients
+						</NavLink>
+						<NavLink
+							to="/appointments"
+							className={styles.nav_link}
+							activeClassName={styles.nav_link_active}
+						>
+							{window.location.pathname === "/appointments" ? (
+								<img
+									src={AppointmentsActive}
+									alt="active appointments icon"
+								/>
+							) : (
+								<img
+									src={AppointmentsInactive}
+									alt="inactive appointments icon"
+								/>
+							)}
+							Appointments
+						</NavLink>
+						<div
+							className={styles.more_div}
+							onClick={() => this.toggleMoreMenu()}
+						>
+							<img src={MoreIcon} alt="more icon" />
+							<span>More</span>
+						</div>
+						{this.state.moreMenuShowing ? <MoreMenu /> : null}
+					</div>
+					<div className={styles.avatar_div}>
+						<img src={TopBarAvatar} alt="avatar" />
+						<span>Dr. Nuel</span>
+					</div>
+				</>
+			</div>
+		);
+	}
+}
+
+export default Topbar;
