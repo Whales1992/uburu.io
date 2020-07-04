3fb3b116d6fb149056c8ba0b00a3b6b531f3c79c
Daniel Ngozika <danielngozikac@gmail.com>
2020-07-04 10:50:23 +0100

    Add institution name.

--- src/components/Home/JS/blue_backdrop.js
+++ src/components/Home/JS/blue_backdrop.js
@@ -1,6 +1,10 @@
 import React from "react";
 import styles from "../CSS/blue_backdrop.module.css";
 
-const blueBackdrop = () => <div className={styles.div}></div>;
+const blueBackdrop = () => (
+	<div className={styles.div}>
+		{JSON.parse(localStorage.account).Description}
+	</div>
+);
 
 export default blueBackdrop;
