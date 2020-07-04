8320c37decf7c4c83e999c9ce85aeacd14a17c42
Daniel Ngozika <danielngozikac@gmail.com>
2020-07-04 11:22:55 +0100

    Include hospital icon and restructure div.

--- src/components/Home/JS/blue_backdrop.js
+++ src/components/Home/JS/blue_backdrop.js
@@ -1,9 +1,12 @@
 import React from "react";
 import styles from "../CSS/blue_backdrop.module.css";
 
+import HospitalIcon from "../../../images/Home/hospital_icon.svg";
+
 const blueBackdrop = () => (
 	<div className={styles.div}>
-		{JSON.parse(localStorage.account).Description}
+		<img alt="hospital icon" src={HospitalIcon} />
+		<span>{JSON.parse(localStorage.account).Description}</span>
 	</div>
 );
 
