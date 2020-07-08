e34e98952e23c006dc6b7a9e093f4ea86e67b46f
Daniel Ngozika <danielngozikac@gmail.com>
2020-07-08 11:57:44 +0100

    Create institution banner.

---
+++ src/components/UI/JS/institution_banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../CSS/institution_banner.module.css";
+import HospitalDesktopIcon from "../../../images/Home/hospital_icon_desktop.svg";
+
+const institutionBanner = () => (
+	<div className={styles.banner}>
+		<img src={HospitalDesktopIcon} alt="hospital icon" />
+		<span>{JSON.parse(localStorage.account).Description}</span>
+	</div>
+);
+
+export default institutionBanner;
