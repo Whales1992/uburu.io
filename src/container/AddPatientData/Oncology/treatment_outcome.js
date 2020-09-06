e883d91130881412fb10b3a8a1d190c5172e21bf
Daniel Ngozika <danielngozikac@gmail.com>
2020-09-06 23:31:07 +0100

    Implement the new treatment outcome.

--- src/container/AddPatientData/Oncology/treatment_outcome.js
+++ src/container/AddPatientData/Oncology/treatment_outcome.js
@@ -1,161 +1,237 @@
 import React, { Component } from "react";
+import DatePicker from "react-date-picker";
+import localforage from "localforage";
 import SecondaryBar from "../../../components/UI/JS/secondary_navbar";
 import TopBar from "../../../components/UI/JS/topbar";
 import Shell from "../../../components/AddPatientData/JS/shell";
 
 //style
 import styles from "../CSS/add_patient_data.module.css";
+import styles2 from "../CSS/medical_history.module.css";
 
 class TreatmentOutcome extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			good_clinical_response:
-				localStorage.good_clinical_response === "true" ? true : false,
-			poor_clinical_response:
-				localStorage.poor_clinical_response === "true" ? true : false,
-			complete_remission:
-				localStorage.complete_remission === "true" ? true : false,
-			disease_progression:
-				localStorage.disease_progression === "true" ? true : false,
-			alive_and_stable:
-				localStorage.alive_and_stable === "true" ? true : false,
-			died_on_treatment:
-				localStorage.died_on_treatment === "true" ? true : false,
-			died_after_treatment:
-				localStorage.died_after_treatment === "true" ? true : false,
+			treatmentOutcome: "",
+			date: ""
 		};
 
 		this.handleChange = this.handleChange.bind(this);
+		this.resetRecord = this.resetRecord.bind(this);
+		this.submitRecord = this.submitRecord.bind(this);
+		this.createPatient = this.createPatient.bind(this);
 	}
 
 	componentDidMount() {
 		window.scrollTo(0, 0);
 	}
 
-	handleChange(e) {
-		const target = e.target;
-		const value =
-			target.type === "checkbox" ? target.checked : target.value;
-		const name = target.name;
+	handleChange(name, e) {
+		let value;
+
+		if (name === "date") {
+			value = e;
+		} else {
+			value = e.target.value;
+		}
+
 		this.setState(() => ({ [name]: value }));
-		localStorage.setItem(`${name}`, `${value}`);
+	}
+
+	resetRecord() {
+		this.setState({
+			treatmentOutcome: "",
+			date: ""
+		});
+	}
+
+	submitRecord(e, recordName) {
+		if (e) e.preventDefault();
+		let localStorageValue =
+			localStorage.getItem(recordName) &&
+			JSON.parse(localStorage.getItem(recordName));
+		if (localStorageValue) {
+			localStorageValue.push(this.state);
+			const lSValueStringified = JSON.stringify(localStorageValue);
+			localStorage.setItem(recordName, lSValueStringified);
+			this.resetRecord();
+		} else {
+			localStorage.setItem(recordName, `${JSON.stringify([this.state])}`);
+			this.resetRecord();
+		}
+	}
+
+	createPatient(recordName) {
+		const { treatmentOutcome } = this.state;
+
+		if (treatmentOutcome) {
+			this.submitRecord(null, recordName);
+		}
+
+		const bioData = JSON.parse(localStorage.getItem("bio_data"));
+		const drugHistory = JSON.parse(localStorage.getItem("drug_history"));
+		const investigationHistory = JSON.parse(
+			localStorage.getItem("investigation_history")
+		);
+		const TreatmentOutcome = JSON.parse(
+			localStorage.getItem("Treatment_Outcome")
+		);
+		const medicalHistory = {
+			assessment: JSON.parse(localStorage.getItem("Assessment")),
+			care: JSON.parse(localStorage.getItem("Care")),
+			complication: JSON.parse(localStorage.getItem("Complication"))
+		};
+
+		let dataObject = {};
+
+		function buildObject() {
+			for (let i = 0; i < arguments.length; i++) {
+				if (arguments[i] === medicalHistory && medicalHistory) {
+					for (const record in medicalHistory) {
+						if (record) {
+							dataObject = {
+								...dataObject,
+								MedicalHistory: {
+									...dataObject.MedicalHistory,
+									[record]: medicalHistory[record]
+								}
+							};
+						} else continue;
+					}
+				}
+				if (arguments[i] === bioData && bioData) {
+					dataObject = {
+						...dataObject,
+						bioData
+					};
+				} else if (arguments[i] === drugHistory && drugHistory) {
+					dataObject = {
+						...dataObject,
+						drugHistory
+					};
+				} else if (
+					arguments[i] === investigationHistory &&
+					investigationHistory
+				) {
+					dataObject = {
+						...dataObject,
+						investigationHistory
+					};
+				} else if (
+					arguments[i] === TreatmentOutcome &&
+					TreatmentOutcome
+				) {
+					dataObject = {
+						...dataObject,
+						TreatmentOutcome
+					};
+				}
+			}
+		}
+
+		buildObject(
+			bioData,
+			drugHistory,
+			investigationHistory,
+			TreatmentOutcome,
+			medicalHistory
+		);
+
+		localforage
+			.setItem(bioData.folder_number, dataObject)
+			.then((value) => {
+				localStorage.removeItem("bio_data");
+				localStorage.removeItem("drug_history");
+				localStorage.removeItem("investigation_history");
+				localStorage.removeItem("Treatment_Outcome");
+				localStorage.removeItem("Assessment");
+				localStorage.removeItem("Care");
+				localStorage.removeItem("Complication");
+				console.log("Successful!");
+				this.props.history.push("/add_patient_data/patient_biodata");
+			})
+			.catch((err) => err);
 	}
 
 	render() {
-		const {
-			good_clinical_response,
-			poor_clinical_response,
-			complete_remission,
-			disease_progression,
-			alive_and_stable,
-			died_on_treatment,
-			died_after_treatment,
-		} = this.state;
+		const { treatmentOutcome, date } = this.state;
 		return (
 			<>
 				<TopBar hide_on_small_screens />
-				<SecondaryBar page_title="Add Patient Data (4/4)" shadow />
+				<SecondaryBar page_title="Treatment Outcome" shadow />
 				<Shell>
-					<form className={styles.form}>
+					<form
+						className={styles.form}
+						onSubmit={(e) =>
+							this.submitRecord(e, "Treatment_Outcome")
+						}
+					>
 						<div className={styles.current_style}>
 							Treatment Outcome
 						</div>
 						<div className={styles.fields}>
-							{!poor_clinical_response ? (
-								<div className={styles.checkbox_div}>
-									<label>Good Clinical Response</label>
-									<input
-										id="good_clinical_response"
-										type="checkbox"
-										name="good_clinical_response"
-										className={styles.input}
-										onChange={(e) => this.handleChange(e)}
-										checked={good_clinical_response}
-									/>
-								</div>
-							) : null}
-							{!good_clinical_response ? (
-								<div className={styles.checkbox_div}>
-									<label>Poor Clinical Response</label>
-									<input
-										id="poor_clinical_response"
-										type="checkbox"
-										name="poor_clinical_response"
-										className={styles.input}
-										onChange={(e) => this.handleChange(e)}
-										checked={poor_clinical_response}
-									/>
-								</div>
-							) : null}
-							{!disease_progression ? (
-								<div className={styles.checkbox_div}>
-									<label>Complete Remission</label>
-									<input
-										id="complete_remission"
-										type="checkbox"
-										name="complete_remission"
-										className={styles.input}
-										onChange={(e) => this.handleChange(e)}
-										checked={complete_remission}
-									/>
-								</div>
-							) : null}
-							{!complete_remission ? (
-								<div className={styles.checkbox_div}>
-									<label>Disease Progression</label>
-									<input
-										id="disease_progression"
-										type="checkbox"
-										name="disease_progression"
-										className={styles.input}
-										onChange={(e) => this.handleChange(e)}
-										checked={disease_progression}
-									/>
-								</div>
-							) : null}
-							{died_after_treatment ||
-							died_on_treatment ? null : (
-								<div className={styles.checkbox_div}>
-									<label>Alive and Well</label>
-									<input
-										id="alive_and_stable"
-										type="checkbox"
-										name="alive_and_stable"
-										className={styles.input}
-										onChange={(e) => this.handleChange(e)}
-										checked={alive_and_stable}
-									/>
-								</div>
-							)}
-							{alive_and_stable || died_after_treatment ? null : (
-								<div className={styles.checkbox_div}>
-									<label>Died on Treatment</label>
-									<input
-										id="died_on_treatment"
-										type="checkbox"
-										name="died_on_treatment"
-										className={styles.input}
-										onChange={(e) => this.handleChange(e)}
-										checked={died_on_treatment}
-									/>
-								</div>
-							)}
-							{alive_and_stable || died_on_treatment ? null : (
-								<div className={styles.checkbox_div}>
-									<label>Died after Treatment</label>
-									<input
-										id="died_after_treatment"
-										type="checkbox"
-										name="died_after_treatment"
-										className={styles.input}
-										onChange={(e) => this.handleChange(e)}
-										checked={died_after_treatment}
-									/>
-								</div>
-							)}
+							<div>
+								<label htmlFor="outcome">
+									Treatment Outcome
+								</label>
+								<select
+									id="outcome"
+									name="treatment outcome"
+									className={styles.input}
+									value={treatmentOutcome}
+									onChange={(e) =>
+										this.handleChange("treatmentOutcome", e)
+									}
+									required
+								>
+									<option></option>
+									<option>Chest X-ray</option>
+									<option>Ultrasound</option>
+									<option>CT-Scan</option>
+									<option>Full Blood Count</option>
+									<option>CA 15-3</option>
+									<option>CA 125</option>
+									<option>CA 27-29</option>
+									<option>CEA</option>
+									<option>PSA</option>
+								</select>
+							</div>
+							<div>
+								<label
+									className={
+										!treatmentOutcome
+											? "disabled_label"
+											: ""
+									}
+								>
+									Date of Record
+								</label>
+								<DatePicker
+									name="date"
+									className={styles.input}
+									onChange={(e) =>
+										this.handleChange("date", e)
+									}
+									value={date}
+									format="dd/MM/y"
+									required
+									disabled={!treatmentOutcome}
+								/>
+							</div>
 						</div>
+						<button
+							type="submit"
+							className={
+								!treatmentOutcome || !date
+									? styles2.submit_btn_disabled
+									: styles2.submit_btn
+							}
+							disabled={!treatmentOutcome || !date}
+						>
+							Add New Record
+						</button>
 						<div className={styles.btn_area}>
 							<button
 								className="secondary_btn"
@@ -164,7 +240,14 @@ class TreatmentOutcome extends Component {
 							>
 								Back
 							</button>
-							<button className="primary_btn" type="submit">
+							<button
+								className="primary_btn"
+								type="button"
+								disabled={treatmentOutcome && !date}
+								onClick={() =>
+									this.createPatient("Treatment_Outcome")
+								}
+							>
 								Submit
 							</button>
 						</div>
