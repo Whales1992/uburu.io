e32777c390eb1e06b240a486f1b1427e2b2390a8
Daniel Ngozika <danielngozikac@gmail.com>
2020-09-06 05:11:53 +0100

    Pass the patient biodata function.

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ const App = () => {
 					/>
 					<Route
 						path="/add_patient_data/patient_biodata"
-						component={PatientBiodata}
+						component={PatientBiodata()}
 					/>
 					<Route
 						path="/add_patient_data/medical_history"
