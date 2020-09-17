b088c846c0888756f2a0f01ecb746de90bd55aff
Daniel Ngozika <danielngozikac@gmail.com>
2020-09-17 17:57:26 +0100

    Implement the search folder number page.

--- src/components/SearchFolderNo/JS/search_folder_no.js
+++ src/components/SearchFolderNo/JS/search_folder_no.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import localForage from "localforage";
 import styles from "../CSS/search_folder_no.module.css";
 import SecondaryBar from "../../../components/UI/JS/secondary_navbar";
 import Title from "../../../components/UI/JS/title";
@@ -7,12 +8,29 @@ import Topbar from "../../../components/UI/JS/topbar";
 import BottomBar from "../../UI/JS/bottom_toolbar";
 import InstitutionBanner from "../../UI/JS/institution_banner";
 import AddPatientIcon from "../../../images/SearchPatientNo/add_patient.svg";
+// import Backdrop from "../../UI/JS/backdrop";
+// import ResultModal from "./search_results_modal";
 
 const SearchFolderNoPage = () => {
 	const [value, changeValue] = useState("");
+	const [searchResult, setSearchResult] = useState("");
+	const [modalShowing, toggleModal] = useState(false);
 
 	function handleChange(e) {
 		changeValue(e);
+	}
+
+	// function hideShowModal() {
+	// 	toggleModal(!modalShowing);
+	// }
+
+	function search(e) {
+		e.preventDefault();
+		localForage.getItem(value).then((patient) => {
+			if (!patient) return setSearchResult(null);
+			setSearchResult(patient);
+			toggleModal(!modalShowing);
+		});
 	}
 
 	return (
@@ -21,7 +39,13 @@ const SearchFolderNoPage = () => {
 			<InstitutionBanner />
 			<Title title="Search Patient Folder" />
 			<SecondaryBar page_title="Search Patient folder" shadow />
-			<form className={styles.form}>
+			{/* {modalShowing ? (
+				<>
+					<Backdrop onClick={hideShowModal} />
+					<ResultModal result={searchResult} />
+				</>
+			) : null} */}
+			<form className={styles.form} onSubmit={(e) => search(e)}>
 				<label>Search Patient folder</label>
 				<input
 					className={styles.input}
@@ -31,7 +55,17 @@ const SearchFolderNoPage = () => {
 					placeholder="Input Patient Number"
 					onChange={(e) => handleChange(e.target.value)}
 				/>
-				<Link to="/add_patient_data/patient_biodata">
+				<button
+					type="submit"
+					className={["primary_btn", styles.search_btn].join(" ")}
+					disabled={!value}
+				>
+					Search
+				</button>
+				<Link
+					to="/add_patient_data/patient_biodata"
+					className={styles.add_new_patient_link}
+				>
 					<img
 						rel="preload"
 						src={AddPatientIcon}
@@ -39,7 +73,41 @@ const SearchFolderNoPage = () => {
 					/>{" "}
 					Add New Patient
 				</Link>
+				{searchResult && (
+					<>
+						<Link
+							to={{
+								pathname: `/patient_detail/${searchResult.bioData.folder_number}`,
+								state: searchResult
+							}}
+							className={styles.result}
+						>{`${searchResult.bioData.first_name} ${searchResult.bioData.surname}`}</Link>
+						<Link
+							to="/add_patient_data/patient_biodata"
+							className={styles.add_new_patient_link_mobile}
+						>
+							<img
+								rel="preload"
+								src={AddPatientIcon}
+								alt="add patient icon"
+							/>{" "}
+							Add New Patient
+						</Link>
+					</>
+				)}
+				{searchResult === null && (
+					<p className={styles.not_found}>No result found.</p>
+				)}
 			</form>
+			{/* <div className={styles.result}>
+				{searchResult ? (
+					<Link to={`/patient_detail/${searchResult.folder_number}`}>
+						{searchResult.first_name + " " + searchResult.surname}
+					</Link>
+				) : (
+					"No patient found"
+				)}
+			</div> */}
 			<BottomBar />
 		</>
 	);
---
+++ src/components/SearchFolderNo/JS/search_results_modal.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../CSS/search_results_modal.module.css";
+
+const SearchResultsModal = ({ result }) => {
+	const { first_name, surname, folder_number } = result;
+	return (
+		<div className={styles.modal_div}>
+			<p>Result:</p>
+			<div className={styles.result}>
+				{result ? (
+					<Link to={`/patient_detail/${folder_number}`}>
+						{first_name + " " + surname}
+					</Link>
+				) : (
+					"No patient found"
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default SearchResultsModal;
