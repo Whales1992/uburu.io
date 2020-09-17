4a318c2923b4f950f14b640ea0eafc32c01003c4
Daniel Ngozika <danielngozikac@gmail.com>
2020-09-17 04:23:20 +0100

    Implement new Patient page.

--- src/components/Patients/JS/records.js
+++ src/components/Patients/JS/records.js
@@ -1,49 +1,51 @@
 import React, { memo } from "react";
+import { Link } from "react-router-dom";
 import styles from "../CSS/records.module.css";
-import { withRouter } from "react-router-dom";
 
-export const EachRecentRecord = ({ record, detail, redirect }) => {
-	const { name, disease, date } = record;
-	function clicked() {
-		detail(record);
-		if (document.URL.match(`${window.location.host}/patients`))
-			return redirect.push(`/patient_detail/${record.id}`);
-		if (document.URL.match(`${window.location.host}/appointments`))
-			return redirect.push(`/appointment_detail/${record.id}`);
-	}
+export const EachRecentRecord = ({ record }) => {
+	const {
+		surname,
+		first_name,
+		organ_diagnosis,
+		folder_number
+	} = record.bioData;
+
 	return (
-		<div className={styles.each_record} onClick={() => clicked()}>
-			<div className={styles.name}>{name}</div>
+		<Link
+			to={{ pathname: `/patient_detail/${folder_number}`, state: record }}
+			className={styles.each_record}
+		>
+			<div className={styles.name}>{`${surname} ${first_name}`}</div>
 			<div className={styles.details}>
-				<small>{disease}</small>
-				<small>{date}</small>
+				<small>{organ_diagnosis}</small>
+				<small>{}</small>
 			</div>
 			<hr />
-		</div>
+		</Link>
 	);
 };
 
-const recentRecords = ({ recents, detail, history }) => {
+const RecentRecords = ({ recents }) => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.records_div}>
 				{recents === null ? (
 					<p>loading...</p>
 				) : recents.length === 0 ? (
-					<p>No records yet.</p>
+					<p style={{ textAlign: "center" }}>No records.</p>
 				) : (
-					recents.map((eachRecord) => (
-						<EachRecentRecord
-							key={eachRecord.name}
-							record={eachRecord}
-							detail={detail}
-							redirect={history}
-						/>
-					))
+					<>
+						{recents.map((eachRecord) => (
+							<EachRecentRecord
+								key={eachRecord.folder_number}
+								record={eachRecord}
+							/>
+						))}
+					</>
 				)}
 			</div>
 		</div>
 	);
 };
 
-export default memo(withRouter(recentRecords));
+export default memo(RecentRecords);
--- src/container/Patients/JS/patients.js
+++ src/container/Patients/JS/patients.js
@@ -1,161 +1,115 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
+import localForage from "localforage";
 
 //components
 import Layout from "../../UI/JS/layout";
-import Tab from "../../../components/UI/JS/tab";
 import Records from "../../../components/Patients/JS/records";
+import Title from "../../../components/UI/JS/title";
+import InstitutionBanner from "../../../components/UI/JS/institution_banner";
 
 //style
 import styles from "../../Patients/CSS/patients.module.css";
-
-//actions
-import { detailSubject } from "../../../actions/general/index";
-
-const diseaseCategories = ["Diabetes", "HBP", "Prostrate"];
-const patientsArray1 = [
-	{
-		id: 1,
-		name: "Johnson Uzodimma",
-		disease: "Diabetes Meningitis 11",
-		date: "30 JAN, 2020",
-	},
-	{
-		id: 2,
-		name: "Courtney Fox",
-		disease: "Diabetes Meningitis 11",
-		date: "30 JAN, 2020",
-	},
-	{
-		id: 3,
-		name: "Cody Watson",
-		disease: "Diabetes Meningitis 11",
-		date: "30 JAN, 2020",
-	},
-	{
-		id: 4,
-		name: "Eduardo Cooper",
-		disease: "Diabetes Meningitis 11",
-		date: "30 JAN, 2020",
-	},
-];
-const patientsArray2 = [
-	{
-		id: 5,
-		name: "Ada Uzodimma",
-		disease: "Diabetes Meningitis 11",
-		date: "30 JAN, 2020",
-	},
-	{
-		id: 6,
-		name: "Courtney Fox",
-		disease: "Diabetes Meningitis 11",
-		date: "30 JAN, 2020",
-	},
-	{
-		id: 7,
-		name: "Cody Watson",
-		disease: "Diabetes Meningitis 11",
-		date: "30 JAN, 2020",
-	},
-	{
-		id: 8,
-		name: "Eduardo Cooper",
-		disease: "Diabetes Meningitis 11",
-		date: "30 JAN, 2020",
-	},
-];
-const patientsArray3 = [
-	{
-		id: 9,
-		name: "Okereke Uzodimma",
-		disease: "Diabetes Meningitis 11",
-		date: "30 JAN, 2020",
-	},
-	{
-		id: 10,
-		name: "Courtney Fox",
-		disease: "Diabetes Meningitis 11",
-		date: "30 JAN, 2020",
-	},
-	{
-		id: 11,
-		name: "Cody Watson",
-		disease: "Diabetes Meningitis 11",
-		date: "30 JAN, 2020",
-	},
-	{
-		id: 12,
-		name: "Eduardo Cooper",
-		disease: "Diabetes Meningitis 11",
-		date: "30 JAN, 2020",
-	},
-];
 
 class Patients extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			diseaseCategories: null,
-			activeTab: "",
-			patients: patientsArray1,
+			patients: null,
 			loading: false,
+			searchValue: ""
 		};
 
-		this.switchTabs = this.switchTabs.bind(this);
+		this.onChange = this.onChange.bind(this);
+		this.search = this.search.bind(this);
 	}
 
-	componentDidMount() {
-		this.setState({ diseaseCategories });
-		this.setState({ activeTab: diseaseCategories[0] });
+	async componentDidMount() {
+		try {
+			let patients = [];
+
+			await localForage.iterate((value) => {
+				patients.push(value);
+			});
+			this.setState({ patients });
+		} catch (error) {
+			console.log(error);
+		}
 	}
 
-	switchTabs(tab) {
-		this.setState({ activeTab: tab }, () => {
-			const { activeTab } = this.state;
+	async onChange(e) {
+		e.preventDefault();
+		const val = e.target.value;
+		if (val === "") {
+			try {
+				let patients = [];
 
-			if (activeTab === "Diabetes")
-				return this.setState(() => ({ patients: patientsArray1 }));
-			if (activeTab === "HBP")
-				return this.setState(() => ({ patients: patientsArray2 }));
-			if (activeTab === "Prostrate")
-				return this.setState(() => ({ patients: patientsArray3 }));
-		});
+				await localForage.iterate((value) => {
+					patients.push(value);
+				});
+				this.setState({ patients });
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		this.setState({ searchValue: val });
+	}
+
+	async search(e) {
+		e.preventDefault();
+		const value = e.target.value;
+		try {
+			let patients = [];
+
+			const foundPatient = await localForage.getItem(value);
+			patients.push(foundPatient);
+
+			if (foundPatient === null) {
+				return this.setState({ patients: [] });
+			}
+			this.setState({ patients });
+		} catch (error) {
+			console.log(error);
+		}
 	}
 
 	render() {
-		const { diseaseCategories, activeTab, patients } = this.state;
+		const { patients, searchValue } = this.state;
 		return (
 			<Layout pageTitle="Your Patients">
-				<Tab>
-					{diseaseCategories
-						? diseaseCategories.map((eachType) => (
-								<div
-									onClick={() => this.switchTabs(eachType)}
-									key={eachType}
-									className={
-										activeTab === eachType
-											? [
-													styles.each_tab,
-													styles.tab_active,
-											  ].join(" ")
-											: styles.each_tab
-									}
-								>
-									{eachType}
-								</div>
-						  ))
-						: null}
-				</Tab>
-				<Records recents={patients} detail={this.props.detailSubject} />
+				<InstitutionBanner />
+				<Title title="Your Patients" />
+				<form className={styles.form} onSubmit={(e) => this.search(e)}>
+					<input
+						className={styles.input}
+						type="number"
+						name="search"
+						value={searchValue}
+						placeholder="Search"
+						onChange={(e) => this.onChange(e)}
+					/>
+					<button
+						type="submit"
+						aria-label="search"
+						disabled={!searchValue}
+					>
+						<svg
+							width="18"
+							height="18"
+							viewBox="0 0 18 18"
+							fill="none"
+						>
+							<path
+								d="M16.8395 15.4605L13.1641 11.7852C14.0489 10.6072 14.5266 9.17331 14.525 7.7C14.525 3.93672 11.4633 0.875 7.7 0.875C3.93672 0.875 0.875 3.93672 0.875 7.7C0.875 11.4633 3.93672 14.525 7.7 14.525C9.17331 14.5266 10.6072 14.0489 11.7852 13.1641L15.4605 16.8395C15.6466 17.0058 15.8893 17.0945 16.1387 17.0876C16.3881 17.0806 16.6255 16.9784 16.8019 16.8019C16.9784 16.6255 17.0806 16.3881 17.0876 16.1387C17.0945 15.8893 17.0058 15.6466 16.8395 15.4605ZM2.825 7.7C2.825 6.73582 3.11091 5.79329 3.64659 4.9916C4.18226 4.18991 4.94363 3.56506 5.83442 3.19609C6.72521 2.82711 7.70541 2.73057 8.65107 2.91867C9.59672 3.10678 10.4654 3.57107 11.1471 4.25285C11.8289 4.93464 12.2932 5.80328 12.4813 6.74894C12.6694 7.69459 12.5729 8.67479 12.2039 9.56558C11.8349 10.4564 11.2101 11.2177 10.4084 11.7534C9.60672 12.2891 8.66418 12.575 7.7 12.575C6.40755 12.5735 5.16847 12.0593 4.25457 11.1454C3.34066 10.2315 2.82655 8.99246 2.825 7.7Z"
+								fill="#A7A9BC"
+							/>
+						</svg>
+					</button>
+				</form>
+				<Records recents={patients} />
 			</Layout>
 		);
 	}
 }
 
-const mapDispatchToProps = (dispatch) => ({
-	detailSubject: (arg) => dispatch(detailSubject(arg)),
-});
-
-export default connect(null, mapDispatchToProps)(Patients);
+export default Patients;
