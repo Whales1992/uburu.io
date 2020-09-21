43271c2e96a60de8bb07e7f8ce0738d1b7c3f6dc
Daniel Ngozika <danielngozikac@gmail.com>
2020-09-21 10:52:00 +0100

    Correctly pick out facility name from localStorage.

--- src/components/Home/JS/blue_backdrop.js
+++ src/components/Home/JS/blue_backdrop.js
@@ -6,7 +6,7 @@ import HospitalIcon from "../../../images/Home/hospital_icon.svg";
 const blueBackdrop = () => (
 	<div className={styles.div}>
 		<img alt="hospital icon" src={HospitalIcon} />
-		<span>{JSON.parse(localStorage.account).Description}</span>
+		<span>{JSON.parse(localStorage.account).FacilityName}</span>
 	</div>
 );
 
--- src/components/UI/JS/institution_banner.js
+++ src/components/UI/JS/institution_banner.js
@@ -5,7 +5,7 @@ import HospitalDesktopIcon from "../../../images/Home/hospital_icon_desktop.svg"
 const institutionBanner = () => (
 	<div className={styles.banner}>
 		<img src={HospitalDesktopIcon} alt="hospital icon" />
-		<span>{JSON.parse(localStorage.account).Description}</span>
+		<span>{JSON.parse(localStorage.account).FacilityName}</span>
 	</div>
 );
 
