991fb3cad14ab606eacb416662afc18214a3c86c
Daniel Ngozika <danielngozikac@gmail.com>
2020-10-03 13:22:17 +0100

    Fix search on folder number search page.

--- src/components/SearchFolderNo/JS/search_folder_no.js
+++ src/components/SearchFolderNo/JS/search_folder_no.js
@@ -11,12 +11,24 @@ import AddPatientIcon from "../../../images/SearchPatientNo/add_patient.svg";
 // import Backdrop from "../../UI/JS/backdrop";
 // import ResultModal from "./search_results_modal";
 
+const url = process.env.REACT_APP_BASE_URL;
+
 const SearchFolderNoPage = () => {
 	const [value, changeValue] = useState("");
+	const [effects, setEffects] = useState({
+		loading: false,
+		error: {
+			error: false,
+			message: ""
+		}
+	});
 	const [searchResult, setSearchResult] = useState("");
 	const [modalShowing, toggleModal] = useState(false);
 
 	function handleChange(e) {
+		if (e === "") {
+			setSearchResult("");
+		}
 		changeValue(e);
 	}
 
@@ -24,20 +36,64 @@ const SearchFolderNoPage = () => {
 	// 	toggleModal(!modalShowing);
 	// }
 
-	function search(e) {
+	async function search(e) {
 		e.preventDefault();
-		localForage.getItem(value).then((patient) => {
-			if (!patient) return setSearchResult(null);
-			setSearchResult(patient);
-			toggleModal(!modalShowing);
-		});
+		try {
+			setEffects({ ...effects, loading: true });
+			if (window.navigator.onLine) {
+				const request = await fetch(`${url}/GetOnePatient`, {
+					method: "POST",
+					headers: {
+						Accept: "application/json",
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${localStorage.token}`
+					},
+					body: JSON.stringify({ FolderNo: value })
+				});
+
+				if (!request.ok) {
+					setEffects({ ...effects, loading: false });
+					const error = await request.json();
+					throw Error(error.error);
+				}
+
+				const data = await request.json();
+				setSearchResult(data.Patients[0]);
+				setEffects({ ...effects, loading: false });
+			} else {
+				localForage.getItem(value).then((patient) => {
+					if (!patient) return setSearchResult(null);
+					setSearchResult(patient);
+					toggleModal(!modalShowing);
+					setEffects({ ...effects, loading: false });
+				});
+			}
+		} catch (error) {
+			setEffects({
+				...effects,
+				error: {
+					error: true,
+					message: error.message
+				}
+			});
+
+			setTimeout(() => {
+				setEffects({
+					error: {
+						error: false,
+						message: ""
+					}
+				});
+				changeValue("");
+			}, 3000);
+		}
 	}
 
 	return (
 		<>
 			<Topbar />
 			<InstitutionBanner />
-			<Title banner title="Search Patient Folder" />
+			<Title banner title="Enter Patient Folder" />
 			<SecondaryBar page_title="Search Patient folder" shadow />
 			{/* {modalShowing ? (
 				<>
@@ -46,14 +102,14 @@ const SearchFolderNoPage = () => {
 				</>
 			) : null} */}
 			<form className={styles.form} onSubmit={(e) => search(e)}>
-				<label htmlFor="search">Search Patient folder</label>
+				<label htmlFor="search">Enter Patient folder</label>
 				<input
 					id="search"
 					className={styles.input}
 					name="search_folder_no"
 					value={value}
 					type="number"
-					placeholder="Input Patient Number"
+					placeholder="Input Patient Folder Number"
 					onChange={(e) => handleChange(e.target.value)}
 				/>
 				<button
@@ -80,12 +136,12 @@ const SearchFolderNoPage = () => {
 							to={{
 								pathname:
 									window.innerWidth > 600
-										? `/patients/${searchResult.bioData.folder_number}/bio-data`
-										: `/patients/${searchResult.bioData.folder_number}/record_list`,
+										? `/patients/${searchResult.FolderNo}/bio-data`
+										: `/patients/${searchResult.FolderNo}/record_list`,
 								state: searchResult
 							}}
 							className={styles.result}
-						>{`${searchResult.bioData.first_name} ${searchResult.bioData.surname}`}</Link>
+						>{`${searchResult.FirstName} ${searchResult.LastName}`}</Link>
 						<Link
 							to="/add_patient_data/patient_biodata"
 							className={styles.add_new_patient_link_mobile}
@@ -114,6 +170,14 @@ const SearchFolderNoPage = () => {
 							Add New Patient
 						</Link>
 					</>
+				)}
+				{effects.loading && (
+					<p style={{ textAlign: "center" }}>Loading...</p>
+				)}
+				{effects.error.error && (
+					<p style={{ color: "red", textAlign: "center" }}>
+						{effects.error.message}
+					</p>
 				)}
 			</form>
 			{/* <div className={styles.result}>
