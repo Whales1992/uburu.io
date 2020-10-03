4c25919bcda361118bd5699dcd59a2a107381917
Daniel Ngozika <danielngozikac@gmail.com>
2020-10-03 08:07:03 +0100

    Implement logout.

--- src/components/Settings/JS/settings.js
+++ src/components/Settings/JS/settings.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import styles from "../CSS/settings.module.css";
 import SecondaryBar from "../../UI/JS/secondary_navbar";
 import CheckUpdates from "../../../images/Settings/check_updates.svg";
@@ -10,46 +10,55 @@ import ViewUsers from "../../../images/Settings/view_users.svg";
 import Topbar from "../../UI/JS/topbar";
 import Title from "../../../components/UI/JS/title";
 
-const settingsPage = () => (
-	<>
-		<Topbar />
-		<SecondaryBar shadow page_title="Settings" />
-		<Title title="Settings" />
-		<div className={styles.settings_menu}>
-			<div className={styles.menu_item}>
-				<img src={Upload} alt="upload to the cloud icon" />
-				<div>
-					<span>Sync Data Online</span>
-					<span>Last sync date: 18/04/2020</span>
-				</div>
-			</div>
-			<Link to="/create_user" className={styles.menu_item}>
-				<img src={AddUser} alt="add user icon" />
-				<div>
-					<span>Create User</span>
-					<span>Create a new Doctor profile</span>
-				</div>
-			</Link>
-			<Link to="/users" className={styles.menu_item}>
-				<img src={ViewUsers} alt="users icon" />
-				<div>
-					<span>View Users</span>
-					<span>List of all Doctors in your Hospital</span>
-				</div>
-			</Link>
-			<div className={styles.menu_item}>
-				<img src={CheckUpdates} alt="phone icon" />
-				<div>
-					<span>Check for Updates</span>
-					<span>Current version: V 1.0</span>
-				</div>
-			</div>
-			<div className={styles.log_out}>
-				<img src={LogOut} alt="power icon" />
-				<span>Log out</span>
-			</div>
-		</div>
-	</>
-);
+const SettingsPage = () => {
+	const history = useHistory();
 
-export default settingsPage;
+	const logout = () => {
+		localStorage.clear();
+		history.push("sign_in");
+	};
+
+	return (
+		<>
+			<Topbar />
+			<SecondaryBar shadow page_title="Settings" />
+			<Title title="Settings" />
+			<div className={styles.settings_menu}>
+				<div className={styles.menu_item}>
+					<img src={Upload} alt="upload to the cloud icon" />
+					<div>
+						<span>Sync Data Online</span>
+						<span>Last sync date: 18/04/2020</span>
+					</div>
+				</div>
+				<Link to="/create_user" className={styles.menu_item}>
+					<img src={AddUser} alt="add user icon" />
+					<div>
+						<span>Create User</span>
+						<span>Create a new Doctor profile</span>
+					</div>
+				</Link>
+				<Link to="/users" className={styles.menu_item}>
+					<img src={ViewUsers} alt="users icon" />
+					<div>
+						<span>View Users</span>
+						<span>List of all Doctors in your Hospital</span>
+					</div>
+				</Link>
+				<div className={styles.menu_item}>
+					<img src={CheckUpdates} alt="phone icon" />
+					<div>
+						<span>Check for Updates</span>
+						<span>Current version: V 1.0</span>
+					</div>
+				</div>
+				<button className={styles.log_out} onClick={() => logout()}>
+					<img src={LogOut} alt="power icon" />
+					<span>Log out</span>
+				</button>
+			</div>
+		</>
+	);
+};
+
+export default SettingsPage;
