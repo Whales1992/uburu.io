fc4e8a50ecc9a0b40c1db88c6c9a9f15ba90e91c
Tega Oboraruvwe <tega.oboraruvwe@hollaport.net>
2020-11-03 22:59:09 +0100

    finished the charts

--- src/components/MedicalAnalytics/JS/PieChart.js
+++ src/components/MedicalAnalytics/JS/PieChart.js
@@ -3,50 +3,22 @@ import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
 
 const data = [
   { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
+  { name: 'Group B', value: 600 },
+  // { name: 'Group C', value: 300 },
+  // { name: 'Group D', value: 200 },
 ];
 
-const COLORS = ['#D1DADD', '#D1DADD', '#242424', '#242424'];
-
-// const RADIAN = Math.PI / 180;
-// const renderCustomizedLabel = ({
-//   cx,
-//   cy,
-//   midAngle,
-//   innerRadius,
-//   outerRadius,
-//   percent,
-//   index,
-// }) => {
-//   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-//   const x = cx + radius * Math.cos(-midAngle * RADIAN);
-//   const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-//   return (
-//     <text
-//       x={x}
-//       y={y}
-//       fill="white"
-//       textAnchor={x > cx ? 'start' : 'end'}
-//       dominantBaseline="central"
-//     >
-//       {`${(percent * 100).toFixed(0)}%`}
-//     </text>
-//   );
-// };
+const COLORS = ['#D1DADD', '#242424'];
 
 const PieChartRight = () => {
   return (
-    <ResponsiveContainer width="100%" height={400}>
+    <ResponsiveContainer width="100%" height={327}>
       <PieChart>
         <Pie
           data={data}
-          cx={200}
-          cy={200}
+          // cx={200}
+          // cy={200}
           labelLine={false}
-          //     label={renderCustomizedLabel}
           outerRadius={80}
           fill="#8884d8"
           dataKey="value"
